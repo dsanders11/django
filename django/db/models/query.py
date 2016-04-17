8dda1d163eded51a67f08a494df0c5c7f2050d81
David Sanders <dsanders11@ucsbalum.com>
2016-04-17 06:43:41 -0700

    Clear annotations on UPDATE query to prevent multiple columns in subqueries

--- django/db/models/query.py
+++ django/db/models/query.py
@@ -558,6 +558,8 @@ class QuerySet(object):
             "Cannot update a query once a slice has been taken."
         self._for_write = True
         query = self.query.clone(sql.UpdateQuery)
+        query.set_annotation_mask(None)
+        query._annotations = None
         query.add_update_values(kwargs)
         with transaction.atomic(using=self.db, savepoint=False):
             rows = query.get_compiler(self.db).execute_sql(CURSOR)
@@ -575,6 +577,8 @@ class QuerySet(object):
         assert self.query.can_filter(), \
             "Cannot update a query once a slice has been taken."
         query = self.query.clone(sql.UpdateQuery)
+        query.set_annotation_mask(None)
+        query._annotations = None
         query.add_update_fields(values)
         self._result_cache = None
         return query.get_compiler(self.db).execute_sql(CURSOR)
